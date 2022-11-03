d8cd9338dcef1638d0ba19b5ec18e5575fc13d02
Adiel Eichenstein <adiel.eich@gmail.com>
2022-11-03 14:19:13 -0400

    commit tree 1744ee6a1d05838315e63cb08a5b1bfbebafc550

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards (array, event ){
+    let newArray = []
+
+for (let a=0; a < array.length; a++) {
+  newArray.push(`Thank you, ${array[a]}, for the wonderful ${event} gift!`)
+debugger;
+}
+    return newArray;
+}
+function countDown(startingNumber){
+while(startingNumber >=0){
+console.log(startingNumber);
+startingNumber--;
+}
+}
+countDown(20)
